da4f3953dfb3b55e6a5534043dd20f9db0fc1721
Billy Gingrich <whgingrich@gmail.com>
2020-04-08 13:20:35 -0400

    initial commit

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,27 @@
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
+import os
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", Item("Bench", "Beautiful wooden bench")),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", Item("Ring", "large diamond ring")),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", Item("Lantern", "old rusty lantern")),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", Item("Map", "old treasure map")),
 }
 
 
@@ -38,6 +41,26 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+player = Player(input'\nWhat is your name? '), room['outside'])
+os.system("clear")
+print(player.current_room)
+
+while True:
+    cmd = input("Cardinal direction ").lower()
+    if cmd in ["n", "s", "e", "w"]:
+        player.travel(cmd)
+        elif cmd == 'i':
+            player.item_inventory()
+        elif cmd == 'l':
+            player.current_room.room_inventory()
+        elif cmd == 'q':
+            print('Cya!')
+            exit()
+        else:
+            print('Command not valid')
+
+
 
 # Write a loop that:
 #
--- src/room.py
+++ src/room.py
@@ -1,2 +1,22 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+from player import Player
+class Room:
+    def __init__(self, name, description, n_to, s_to, e_to, w_to, items=[]):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.items = items
+    def __str__(self):
+        return f' {self.name}\n\n{self.description}'
+
+    def room_inventory(self):
+        if self.items is None:
+            print('No items here')
+        else:
+            for item in self.items:
+                print(f'there is a {item.name}')
