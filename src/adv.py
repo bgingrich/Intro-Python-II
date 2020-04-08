e33d2230c84cad9e2066d51e1d23f1d94f1755ac
Billy Gingrich <whgingrich@gmail.com>
2020-04-08 14:56:10 -0400

    day 1 mvp working

--- src/adv.py
+++ src/adv.py
@@ -9,18 +9,18 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", [Item("Bench", "Beautiful wooden bench")]),
+passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", [Item("Ring", "large diamond ring")]),
+the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", [Item("Lantern", "old rusty lantern")]),
+to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [Item("Map", "old treasure map")]),
+earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -38,28 +38,26 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+username = input("What is your name? ")
 
 # Make a new player object that is currently in the 'outside' room.
-
-player = Player(room['outside'])
+player = Player(username, room['outside'])
 
 # Write a loop that:
-#
 while True:
 
-print("################################")
+    print("##################################################################")
 
 # * Prints the current room name
-print(f'Current Room: {player.room.name}')
+    print(f'Current Room: {player.room.name}')
 
 # * Prints the current description (the textwrap module might be useful here).
-print("################################")
- 	print(f"# {player.room.description}")
-print("################################\n")
+    print("##################################################################")
+    print(f"# {player.room.description}")
+    print("##################################################################\n")
 
 # * Waits for user input and decides what to do.
-#
-user = input("[n] North  [s] South  [e] East  [w] West    [q] Quit:\n").lower()
+    user = input("[n] North  [s] South  [e] East  [w] West    [q] Quit:\n").lower()
 
 # If the user enters "q", quit the game.
     if user == 'q':
@@ -67,28 +65,27 @@ user = input("[n] North  [s] South  [e] East  [w] West    [q] Quit:\n").lower()
 
 # If the user enters a cardinal direction, attempt to move to the room there.
     elif user == 'n':
- 		if player.room.n_to != None:
- 			player.room = player.room.n_to
- 		else:
- 			print("Can't go to the North...")
- 	elif user == 's':
- 		if player.room.s_to != None:
- 			player.room = player.room.s_to
- 		else:
- 			print("Can't go to the South...")
- 	elif user == 'e':
- 		if player.room.e_to != None:
- 			player.room = player.room.e_to
- 		else:
- 			print("Can't go to the East...")
- 	elif user == 'w':
- 		if player.room.w_to != None:
- 			player.room = player.room.w_to
- 		else:
- 			print("Can't go to the West...")
+        if player.room.n_to != None:
+            player.room = player.room.n_to
+        else:
+            print("Can't go to the North...")
+    elif user == 's':
+        if player.room.s_to != None:
+            player.room = player.room.s_to
+        else:
+            print("Can't go to the South...")
+    elif user == 'e':
+        if player.room.e_to != None:
+            player.room = player.room.e_to
+        else:
+            print("Can't go to the East...")
+    elif user == 'w':
+        if player.room.w_to != None:
+            player.room = player.room.w_to
+        else:
+            print("Can't go to the West...")
 
 # Print an error message if the movement isn't allowed.
-#
     else:
         print('Command not valid')
 
--- src/room.py
+++ src/room.py
@@ -1,22 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-from player import Player
 class Room:
-    def __init__(self, name, description, items=[]):
-        self.name = name
-        self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.e_to = None
-        self.w_to = None
-        self.items = items
-    def __str__(self):
-        return f' {self.name}\n\n{self.description}'
-
-    def room_inventory(self):
-        if self.items is None:
-            print('No items here')
-        else:
-            for item in self.items:
-                print(f'there is a {item.name}')
+        def __init__(self, name, description, n_to = None, s_to = None, e_to = None, w_to = None):
+            self.name = name
+            self.description = description
+            self.n_to = n_to
+            self.s_to = s_to
+            self.e_to = e_to
+            self.w_to = w_to
